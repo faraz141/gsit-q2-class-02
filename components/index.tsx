ae632e3e6c6b42971bca6f6607ce4a64cc6cd90e
Faraz Alam <153891083+faraz141@users.noreply.github.com>
2024-09-16 18:21:01 +0500

    add home

--- components/index.tsx
+++ components/index.tsx
@@ -2,7 +2,7 @@ const Index = () => {
   return (
     <div>
       <div className="flex items-center justify-center bg-black opacity-90 text-white h-custom-1">
-        <h1 className="font-bold text-3xl"></h1>
+        <h1 className="font-bold text-3xl">Home </h1>
       </div>
     </div>
   );
